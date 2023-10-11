eb0627854612381fbe6222c3a42dc911904944f5
mahikagup10 <77979481+mahikagup10@users.noreply.github.com>
2023-10-11 09:19:23 -0400

    Add files via upload

---
+++ ecdh/ecc.py
@@ -0,0 +1,9 @@
+# secret key - random integer in a certain range
+# key generation - find random integer in range of 256 bits
+# public key - (x,y) integer coordinates on the curve - compressed to one coordinate plus 1 bit to show odd or even
+# 
+
+import random
+import hashlib
+
+
